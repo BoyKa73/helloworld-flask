7709b5a5fe07cc84eeb94f509700b53c90410843
BoyKa73 <astradyne01@gmail.com>
2025-05-10 12:56:20 +0200

    changed routes /info and /about hinzugefügt

--- app.py
+++ app.py
@@ -13,15 +13,24 @@ def hello_world():
     return 'Hello, World!'
 
 # weitere Route definieren
-@app.route('/hello')
-def hello():
+@app.route('/willkommen')
+def hello_fat():
     return "<h1>Hallo</h1>"
 
 ## TODO: Definiert eine Route auf den Pfad /25-01 mit einer Funktion, die "Hallo Kurs 25-01" zurückgibt
 @app.route('/25-01')
 def kurs():
-    return "<p>Hallo Kurs 25-01</p>"
+    return "<h1>Hallo Kurs 25-01</h1>"
+
+## Dies ist die Hundesalon-Seite
+@app.route('/info')
+def info():
+    return "<h1>Willkommen bei unserem supersmoothen Hundesalon!</h1>"
+## Dies ist die About-us Seite des Salons
+@app.route('/about')
+def about():
+    return 'Dies ist eine Info-Seite über unseren Hunde-Salon.Hier findest Du Infos über uns.'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
